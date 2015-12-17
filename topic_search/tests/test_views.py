922bed818d5e13cad0f9ea8ccf9d6ffa1940aef8
ZachLiuGIS <zachliugis@gmail.com>
2015-12-17 17:24:55 -0500

    added SearchResultViewTest

--- topic_search/tests/test_views.py
+++ topic_search/tests/test_views.py
@@ -13,3 +13,15 @@ class AboutViewTest(TestCase):
     def test_view_render_correct_template(self):
         response = self.client.get('/topic_search/about/')
         self.assertTemplateUsed(response, 'topic_search/about.html')
+
+
+class SearchResultViewTest(TestCase):
+
+    def test_view_render_correct_template(self):
+        response = self.client.get('/topic_search/search-result/?term=text')
+        self.assertTemplateUsed(response, 'topic_search/search_result.html')
+
+    def test_can_display_twitter_result(self):
+        response = self.client.get('/topic_search/search-result/?term=python')
+        self.assertContains(response, "Twitter Search Result")
+        self.assertContains(response, "id_twitter_result_list")
