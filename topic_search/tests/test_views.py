ca50efe2dea5cb516412972d65b6e0d240c8475e
ZachLiuGIS <zachliugis@gmail.com>
2015-12-18 00:00:18 -0500

    updated SearchResultTest, test display results, display empty results, and display search error.

--- topic_search/tests/test_views.py
+++ topic_search/tests/test_views.py
@@ -1,4 +1,8 @@
 from django.test import TestCase
+from unittest.mock import patch
+from topic_search.utils import search_twitter_by_term, TwitterItem
+from datetime import datetime
+from topic_search.utils import TwitterAPIQueryError
 
 
 class HomeViewTest(TestCase):
@@ -21,7 +25,44 @@ class SearchResultViewTest(TestCase):
         response = self.client.get('/topic_search/search-result/?term=text')
         self.assertTemplateUsed(response, 'topic_search/search_result.html')
 
-    def test_can_display_twitter_result(self):
+    @patch('topic_search.views.search_twitter_by_term')
+    def test_can_display_twitter_result(self, mock_search_twitter_by_term):
+
+        items = []
+        for i in range(1, 11):
+            items.append(TwitterItem(
+                text='python ' * i,
+                created_at=datetime.now(),
+                user_name='user_' + str(i),
+                user_screen_name='screenname_' + str(i),
+                profile_url='url_' + str(i)
+            ))
+        mock_search_twitter_by_term.return_value = items
+
         response = self.client.get('/topic_search/search-result/?term=python')
         self.assertContains(response, "Twitter Search Result")
         self.assertContains(response, "id_twitter_result_list")
+        self.assertContains(response, "user_10")
+
+    @patch('topic_search.views.search_twitter_by_term')
+    def test_can_display_twitter_result_return_empty_results(self, mock_search_twitter_by_term):
+
+        items = []
+        mock_search_twitter_by_term.return_value = items
+
+        response = self.client.get('/topic_search/search-result/?term=python')
+        self.assertContains(response, "Twitter Search Result")
+        self.assertNotContains(response, "id_twitter_result_list")
+        self.assertContains(response, "Sorry, no tweets are found for this topic.")
+
+    @patch('topic_search.views.search_twitter_by_term')
+    def test_can_display_twitter_handles_error(self, mock_search_twitter_by_term):
+
+        e = TwitterAPIQueryError()
+        mock_search_twitter_by_term.side_effect = e
+
+        response = self.client.get('/topic_search/search-result/?term=python')
+        self.assertContains(response, "Twitter Search Result")
+        self.assertNotContains(response, "id_twitter_result_list")
+        self.assertContains(response, "Sorry, something is wrong with twitter search")
+
